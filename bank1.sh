c7d3c4e02c76d7c43df6014350dddcdb172cc42b
moon-Light404 <1908328781@qq.com>
2024-12-05 21:13:27 +0800

    ss

--- bank1.sh
+++
@@ -1,6 +0,0 @@
-python main.py --dataset "bank" --pseudo_train 2 --a 0.3
-python main.py --dataset "bank" --pseudo_train 2 --a 0.4
-python main.py --dataset "bank" --pseudo_train 2 --a 0.5
-python main.py --dataset "bank" --pseudo_train 2 --a 0.6
-python main.py --dataset "bank" --pseudo_train 2 --a 0.7
-python main.py --dataset "bank" --pseudo_train 2 --a 0.8
--- cifar.sh
+++
@@ -1,8 +0,0 @@
-
-for n_domins in 2 4 8 16
-do
-  for a in $(seq 0.2 0.1 0.9)
-  do
-    python main.py --iteration 10000 --dlr 3e-4 --pseudo_train 2 --n_domins "$n_domins" --a "$a"
-  done
-done
